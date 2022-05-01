eb96e10d45e35220d81fc9ab3dbdfcbbe4ada2d2
antonioo83 <jurchenko@softbalance.ru>
2022-05-01 14:03:39 +0300

    Migration, 000001_create_tables.up.sql: added;

--- migrations/000001_create_tables.up.sql
+++ migrations/000001_create_tables.up.sql
@@ -5,8 +5,6 @@
 -- Dumped from database version 14.2
 -- Dumped by pg_dump version 14.2
 
--- Started on 2022-04-22 15:16:28
-
 SET statement_timeout = 0;
 SET lock_timeout = 0;
 SET idle_in_transaction_session_timeout = 0;
@@ -23,18 +21,17 @@ SET default_tablespace = '';
 SET default_table_access_method = heap;
 
 --
--- TOC entry 216 (class 1259 OID 25001)
 -- Name: ln_customers; Type: TABLE; Schema: public; Owner: postgres
 --
 
 CREATE TABLE public.ln_customers (
                                      id integer NOT NULL,
                                      user_id integer NOT NULL,
-                                     code character varying(32) DEFAULT ''::character varying NOT NULL,
+                                     code character varying(64) DEFAULT ''::character varying NOT NULL,
                                      type character varying(20) DEFAULT 'service'::character varying NOT NULL,
                                      title character varying(100) DEFAULT ''::character varying NOT NULL,
                                      inn character varying(12) DEFAULT ''::character varying NOT NULL,
-                                     description character varying(32) DEFAULT ''::character varying NOT NULL,
+                                     description character varying(256) DEFAULT ''::character varying NOT NULL,
                                      created_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP NOT NULL,
                                      updated_at timestamp without time zone,
                                      deleted_at timestamp without time zone
@@ -44,7 +41,6 @@ CREATE TABLE public.ln_customers (
 ALTER TABLE public.ln_customers OWNER TO postgres;
 
 --
--- TOC entry 215 (class 1259 OID 25000)
 -- Name: ln_customers_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
@@ -60,8 +56,6 @@ CREATE SEQUENCE public.ln_customers_id_seq
 ALTER TABLE public.ln_customers_id_seq OWNER TO postgres;
 
 --
--- TOC entry 3382 (class 0 OID 0)
--- Dependencies: 215
 -- Name: ln_customers_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
 
@@ -69,7 +63,6 @@ ALTER SEQUENCE public.ln_customers_id_seq OWNED BY public.ln_customers.id;
 
 
 --
--- TOC entry 217 (class 1259 OID 25013)
 -- Name: ln_licenses; Type: TABLE; Schema: public; Owner: postgres
 --
 
@@ -80,19 +73,21 @@ CREATE TABLE public.ln_licenses (
                                     callback_url character varying(500) DEFAULT ''::character varying NOT NULL,
                                     count integer DEFAULT 1 NOT NULL,
                                     license_key character varying(500) DEFAULT ''::character varying NOT NULL,
-                                    registration_at timestamp without time zone NOT NULL,
+                                    registration_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP NOT NULL,
                                     activation_at timestamp without time zone,
                                     expiration_at timestamp without time zone NOT NULL,
                                     deleted_at timestamp without time zone,
                                     duration integer NOT NULL,
-                                    description character varying(500) DEFAULT ''::character varying NOT NULL
+                                    description character varying(500) DEFAULT ''::character varying NOT NULL,
+                                    code character varying(64) NOT NULL,
+                                    callback_attempts integer DEFAULT 0 NOT NULL,
+                                    is_sent_callback smallint DEFAULT 0 NOT NULL
 );
 
 
 ALTER TABLE public.ln_licenses OWNER TO postgres;
 
 --
--- TOC entry 218 (class 1259 OID 25023)
 -- Name: ln_licenses_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
@@ -108,8 +103,6 @@ CREATE SEQUENCE public.ln_licenses_id_seq
 ALTER TABLE public.ln_licenses_id_seq OWNER TO postgres;
 
 --
--- TOC entry 3383 (class 0 OID 0)
--- Dependencies: 218
 -- Name: ln_licenses_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
 
@@ -117,7 +110,6 @@ ALTER SEQUENCE public.ln_licenses_id_seq OWNED BY public.ln_licenses.id;
 
 
 --
--- TOC entry 212 (class 1259 OID 24983)
 -- Name: ln_user_actions; Type: TABLE; Schema: public; Owner: postgres
 --
 
@@ -134,7 +126,6 @@ CREATE TABLE public.ln_user_actions (
 ALTER TABLE public.ln_user_actions OWNER TO postgres;
 
 --
--- TOC entry 211 (class 1259 OID 24982)
 -- Name: ln_user_actions_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
@@ -150,8 +141,6 @@ CREATE SEQUENCE public.ln_user_actions_id_seq
 ALTER TABLE public.ln_user_actions_id_seq OWNER TO postgres;
 
 --
--- TOC entry 3384 (class 0 OID 0)
--- Dependencies: 211
 -- Name: ln_user_actions_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
 
@@ -159,7 +148,6 @@ ALTER SEQUENCE public.ln_user_actions_id_seq OWNED BY public.ln_user_actions.id;
 
 
 --
--- TOC entry 214 (class 1259 OID 24994)
 -- Name: ln_user_permissions; Type: TABLE; Schema: public; Owner: postgres
 --
 
@@ -177,7 +165,6 @@ CREATE TABLE public.ln_user_permissions (
 ALTER TABLE public.ln_user_permissions OWNER TO postgres;
 
 --
--- TOC entry 209 (class 1259 OID 24966)
 -- Name: ln_users; Type: TABLE; Schema: public; Owner: postgres
 --
 
@@ -197,7 +184,6 @@ CREATE TABLE public.ln_users (
 ALTER TABLE public.ln_users OWNER TO postgres;
 
 --
--- TOC entry 210 (class 1259 OID 24969)
 -- Name: ln_users_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
@@ -213,8 +199,6 @@ CREATE SEQUENCE public.ln_users_id_seq
 ALTER TABLE public.ln_users_id_seq OWNER TO postgres;
 
 --
--- TOC entry 3385 (class 0 OID 0)
--- Dependencies: 210
 -- Name: ln_users_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
 
@@ -222,7 +206,6 @@ ALTER SEQUENCE public.ln_users_id_seq OWNED BY public.ln_users.id;
 
 
 --
--- TOC entry 213 (class 1259 OID 24993)
 -- Name: user_permissions_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
@@ -238,8 +221,6 @@ CREATE SEQUENCE public.user_permissions_id_seq
 ALTER TABLE public.user_permissions_id_seq OWNER TO postgres;
 
 --
--- TOC entry 3386 (class 0 OID 0)
--- Dependencies: 213
 -- Name: user_permissions_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
 
@@ -247,7 +228,6 @@ ALTER SEQUENCE public.user_permissions_id_seq OWNED BY public.ln_user_permission
 
 
 --
--- TOC entry 3197 (class 2604 OID 25004)
 -- Name: ln_customers id; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
@@ -255,7 +235,6 @@ ALTER TABLE ONLY public.ln_customers ALTER COLUMN id SET DEFAULT nextval('public
 
 
 --
--- TOC entry 3209 (class 2604 OID 25024)
 -- Name: ln_licenses id; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
@@ -263,7 +242,6 @@ ALTER TABLE ONLY public.ln_licenses ALTER COLUMN id SET DEFAULT nextval('public.
 
 
 --
--- TOC entry 3191 (class 2604 OID 24986)
 -- Name: ln_user_actions id; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
@@ -271,7 +249,6 @@ ALTER TABLE ONLY public.ln_user_actions ALTER COLUMN id SET DEFAULT nextval('pub
 
 
 --
--- TOC entry 3194 (class 2604 OID 24997)
 -- Name: ln_user_permissions id; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
@@ -279,33 +256,66 @@ ALTER TABLE ONLY public.ln_user_permissions ALTER COLUMN id SET DEFAULT nextval(
 
 
 --
--- TOC entry 3184 (class 2604 OID 24970)
 -- Name: ln_users id; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
 ALTER TABLE ONLY public.ln_users ALTER COLUMN id SET DEFAULT nextval('public.ln_users_id_seq'::regclass);
 
+
 --
--- TOC entry 3387 (class 0 OID 0)
--- Dependencies: 215
+-- Data for Name: ln_customers; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+
+
+--
+-- Data for Name: ln_licenses; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+
+
+--
+-- Data for Name: ln_user_actions; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+INSERT INTO public.ln_user_actions (id, action, description, created_at, updated_at, deleted_at) VALUES (3, 'delete', '', '2022-04-19 10:21:07.563603', '1000-01-01 00:00:00', '1000-01-01 00:00:00');
+INSERT INTO public.ln_user_actions (id, action, description, created_at, updated_at, deleted_at) VALUES (2, 'update', '', '2022-04-19 10:21:07.563603', '1000-01-01 00:00:00', '1000-01-01 00:00:00');
+INSERT INTO public.ln_user_actions (id, action, description, created_at, updated_at, deleted_at) VALUES (1, 'create', '', '2022-04-19 10:21:07.563603', '1000-01-01 00:00:00', '1000-01-01 00:00:00');
+INSERT INTO public.ln_user_actions (id, action, description, created_at, updated_at, deleted_at) VALUES (4, 'get', '', '2022-04-19 10:21:07.563603', '1000-01-01 00:00:00', '1000-01-01 00:00:00');
+
+
+--
+-- Data for Name: ln_user_permissions; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+INSERT INTO public.ln_user_permissions (id, user_id, action_id, product_type, created_at, updated_at, deleted_at) VALUES (78, 44, 1, 'courier', '2022-04-21 16:48:37.353374', NULL, NULL);
+INSERT INTO public.ln_user_permissions (id, user_id, action_id, product_type, created_at, updated_at, deleted_at) VALUES (79, 44, 2, 'courier', '2022-04-21 16:48:37.353374', NULL, NULL);
+INSERT INTO public.ln_user_permissions (id, user_id, action_id, product_type, created_at, updated_at, deleted_at) VALUES (80, 44, 3, 'courier', '2022-04-21 16:48:37.353374', NULL, NULL);
+INSERT INTO public.ln_user_permissions (id, user_id, action_id, product_type, created_at, updated_at, deleted_at) VALUES (81, 44, 4, 'courier', '2022-04-21 16:48:37.353374', NULL, NULL);
+
+
+--
+-- Data for Name: ln_users; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+INSERT INTO public.ln_users (id, code, role, title, auth_token, description, created_at, updated_at, deleted_at) VALUES (44, '909fdfea-fff1-4dd5-97f7-612cf9840b82', 'admin', 'Admin', '54d1ba805e2a4891aeac9299b618945e', '', '2022-04-21 16:48:37.351955', NULL, NULL);
+
+
+--
 -- Name: ln_customers_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public.ln_customers_id_seq', 1, false);
+SELECT pg_catalog.setval('public.ln_customers_id_seq', 25, true);
 
 
 --
--- TOC entry 3388 (class 0 OID 0)
--- Dependencies: 218
 -- Name: ln_licenses_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public.ln_licenses_id_seq', 1, false);
+SELECT pg_catalog.setval('public.ln_licenses_id_seq', 32, true);
 
 
 --
--- TOC entry 3389 (class 0 OID 0)
--- Dependencies: 211
 -- Name: ln_user_actions_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
@@ -313,25 +323,20 @@ SELECT pg_catalog.setval('public.ln_user_actions_id_seq', 4, true);
 
 
 --
--- TOC entry 3390 (class 0 OID 0)
--- Dependencies: 210
 -- Name: ln_users_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public.ln_users_id_seq', 44, true);
+SELECT pg_catalog.setval('public.ln_users_id_seq', 47, true);
 
 
 --
--- TOC entry 3391 (class 0 OID 0)
--- Dependencies: 213
 -- Name: user_permissions_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public.user_permissions_id_seq', 81, true);
+SELECT pg_catalog.setval('public.user_permissions_id_seq', 108, true);
 
 
 --
--- TOC entry 3221 (class 2606 OID 25012)
 -- Name: ln_customers ln_customers_pk; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -340,7 +345,14 @@ ALTER TABLE ONLY public.ln_customers
 
 
 --
--- TOC entry 3223 (class 2606 OID 25026)
+-- Name: ln_customers ln_customers_pk_2; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.ln_customers
+    ADD CONSTRAINT ln_customers_pk_2 UNIQUE (user_id, code);
+
+
+--
 -- Name: ln_licenses ln_licenses_pk; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -349,7 +361,6 @@ ALTER TABLE ONLY public.ln_licenses
 
 
 --
--- TOC entry 3216 (class 2606 OID 24990)
 -- Name: ln_user_actions ln_user_actions_pk; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -358,7 +369,6 @@ ALTER TABLE ONLY public.ln_user_actions
 
 
 --
--- TOC entry 3218 (class 2606 OID 25028)
 -- Name: ln_user_permissions ln_user_permissions_pk; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -367,7 +377,6 @@ ALTER TABLE ONLY public.ln_user_permissions
 
 
 --
--- TOC entry 3213 (class 2606 OID 24992)
 -- Name: ln_users ln_users_pk; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -376,7 +385,20 @@ ALTER TABLE ONLY public.ln_users
 
 
 --
--- TOC entry 3214 (class 1259 OID 25060)
+-- Name: ln_licenses_code_customer_id_uindex; Type: INDEX; Schema: public; Owner: postgres
+--
+
+CREATE UNIQUE INDEX ln_licenses_code_customer_id_uindex ON public.ln_licenses USING btree (code, customer_id);
+
+
+--
+-- Name: ln_licenses_license_key_uindex; Type: INDEX; Schema: public; Owner: postgres
+--
+
+CREATE UNIQUE INDEX ln_licenses_license_key_uindex ON public.ln_licenses USING btree (license_key);
+
+
+--
 -- Name: ln_user_actions_action_uindex; Type: INDEX; Schema: public; Owner: postgres
 --
 
@@ -384,7 +406,6 @@ CREATE UNIQUE INDEX ln_user_actions_action_uindex ON public.ln_user_actions USIN
 
 
 --
--- TOC entry 3219 (class 1259 OID 25059)
 -- Name: ln_user_permissions_user_id, action_id_uindex; Type: INDEX; Schema: public; Owner: postgres
 --
 
@@ -392,7 +413,6 @@ CREATE UNIQUE INDEX "ln_user_permissions_user_id, action_id_uindex" ON public.ln
 
 
 --
--- TOC entry 3210 (class 1259 OID 25058)
 -- Name: ln_users_auth_token_uindex; Type: INDEX; Schema: public; Owner: postgres
 --
 
@@ -400,15 +420,13 @@ CREATE UNIQUE INDEX ln_users_auth_token_uindex ON public.ln_users USING btree (a
 
 
 --
--- TOC entry 3211 (class 1259 OID 25062)
--- Name: ln_users_code_uindex; Type: INDEX; Schema: public; Owner: postgres
+-- Name: ln_users_code_deleted_at_uindex; Type: INDEX; Schema: public; Owner: postgres
 --
 
-CREATE UNIQUE INDEX ln_users_code_uindex ON public.ln_users USING btree (code);
+CREATE UNIQUE INDEX ln_users_code_deleted_at_uindex ON public.ln_users USING btree (code, deleted_at);
 
 
 --
--- TOC entry 3226 (class 2606 OID 25034)
 -- Name: ln_customers ln_customers_ln_users_id_fk; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -417,7 +435,6 @@ ALTER TABLE ONLY public.ln_customers
 
 
 --
--- TOC entry 3227 (class 2606 OID 25029)
 -- Name: ln_licenses ln_licenses_ln_customers_id_fk; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -426,7 +443,6 @@ ALTER TABLE ONLY public.ln_licenses
 
 
 --
--- TOC entry 3225 (class 2606 OID 25049)
 -- Name: ln_user_permissions ln_user_permissions_ln_user_actions_id_fk; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -435,15 +451,12 @@ ALTER TABLE ONLY public.ln_user_permissions
 
 
 --
--- TOC entry 3224 (class 2606 OID 25044)
 -- Name: ln_user_permissions ln_user_permissions_ln_users_id_fk; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
 ALTER TABLE ONLY public.ln_user_permissions
     ADD CONSTRAINT ln_user_permissions_ln_users_id_fk FOREIGN KEY (user_id) REFERENCES public.ln_users(id) ON UPDATE CASCADE ON DELETE CASCADE;
 
-
--- Completed on 2022-04-22 15:16:28
 
 --
 -- PostgreSQL database dump complete
